79027f99d9b15f140197bb9edfde10141c6a0832
George Hahn <george@genericmaker.com>
2014-03-23 22:50:23 -0400

    Add alpha channel to color structure

--- Calambri.Interfaces/Color.cs
+++ Calambri.Interfaces/Color.cs
@@ -5,12 +5,18 @@ namespace Calambri.Interfaces
         public byte R;
         public byte G;
         public byte B;
+        public byte A;
 
-        public Color(byte R, byte G, byte B)
+        public Color(byte R, byte G, byte B, byte A)
         {
             this.R = R;
             this.G = G;
             this.B = B;
+            this.A = A;
         }
+
+        public Color(byte R, byte G, byte B)
+            : this(R, G, B, 0)
+        { }
     }
 }
