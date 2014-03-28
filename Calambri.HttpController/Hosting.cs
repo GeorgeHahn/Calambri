57770d6090699de95e252442a4a58f8208041bcd
George Hahn <george@genericmaker.com>
2014-03-27 21:24:44 -0400

    Added NancyFx powererd HTTP controller (just talks MQTT to the rest of the sustem)

---
+++ Calambri.HttpController/Hosting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+
+namespace Calambri.HttpController
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = new NancyHost(new Uri("http://localhost:80"));
+
+            host.Start();
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+
+            host.Stop();
+        }
+    }
+}
---
+++ Calambri.HttpController/RendererControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Calambri.HttpController
+{
+    public class RendererControl: NancyModule
+    {
+        private MqttClient client;
+
+        public RendererControl()
+        {
+            client = new MqttClient("127.0.0.1");
+            client.Connect(new Guid().ToString());
+
+            Get["/"] = (v) =>
+            {
+                return "See <a href=\"on\">on</a> and <a href=\"off\">off</a>";
+            };
+
+            Get["/on"] = _ =>
+            {
+                client.Publish("/lights/standing/state", new[] { (byte)1 });
+
+                return "Lights set to on";
+            };
+
+            Get["/off"] = _ =>
+            {
+                client.Publish("/lights/standing/state", new[] { (byte)0 });
+
+                return "Lights set to off";
+            };
+        }
+    }
+}
